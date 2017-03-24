abd360d2897301672f3d015ffd177c9c0b572ee1
kisenka <qtuzov@gmail.com>
2017-03-24 23:13:28 +0300

    Tests refactoring

--- src/posthtml-svg-fill/lib/posthtml-svg-fill.test.js
+++ src/posthtml-svg-fill/lib/posthtml-svg-fill.test.js
@@ -48,51 +48,39 @@ describe('selectorToMatcher()', () => {
 });
 
 describe('plugin()', () => {
-  it('should do nothing if fill option not provided', () => {
-    return t(
-      null,
-      '<path />',
-      '<path />'
-    );
-  });
+  it('should do nothing if fill option not provided', () => t(
+    null,
+    '<path />',
+    '<path />'
+  ));
 
-  it('should process single tag', () => {
-    return t(
-      { fill: '#f00', selector: 'path' },
-      '<path /><circle />',
-      '<path fill="#f00" /><circle />'
-    );
-  });
+  it('should process single tag', () => t(
+    { fill: '#f00', selector: 'path' },
+    '<path /><circle />',
+    '<path fill="#f00" /><circle />'
+  ));
 
-  it('should process multiple tags', () => {
-    return t(
-      { fill: 'red', selector: 'path, circle' },
-      '<path /><circle />',
-      '<path fill="red" /><circle fill="red" />'
-    );
-  });
+  it('should process multiple tags', () => t(
+    { fill: 'red', selector: 'path, circle' },
+    '<path /><circle />',
+    '<path fill="red" /><circle fill="red" />'
+  ));
 
-  it('should process #id selectors', () => {
-    return t(
-      { fill: 'red', selector: '#id' },
-      '<path id="id" /><circle />',
-      '<path id="id" fill="red" /><circle />'
-    );
-  });
+  it('should process #id selectors', () => t(
+    { fill: 'red', selector: '#id' },
+    '<path id="id" /><circle />',
+    '<path id="id" fill="red" /><circle />'
+  ));
 
-  it('should process .class selectors', () => {
-    return t(
-      { fill: 'red', selector: '.class' },
-      '<path class="class" /><circle />',
-      '<path class="class" fill="red" /><circle />'
-    );
-  });
+  it('should process .class selectors', () => t(
+    { fill: 'red', selector: '.class' },
+    '<path class="class" /><circle />',
+    '<path class="class" fill="red" /><circle />'
+  ));
 
-  it('should overwrite fill attribute', () => {
-    return t(
-      { fill: 'blue' },
-      '<path fill="red" />',
-      '<path fill="blue" />'
-    );
-  });
+  it('should overwrite fill attribute', () => t(
+    { fill: 'blue' },
+    '<path fill="red" />',
+    '<path fill="blue" />'
+  ));
 });
--- src/posthtml-svg-mode/lib/renderer.test.js
+++ src/posthtml-svg-mode/lib/renderer.test.js
@@ -1,19 +1,15 @@
 /* global utils */
 const t = utils.setupPluginTest();
 
-it('should properly render self-closing tags', () => {
-  return t(
-    null,
-    '<path />',
-    '<path />'
-  );
-});
+it('should properly render self-closing tags', () => t(
+  null,
+  '<path />',
+  '<path />'
+));
 
-it('should detect if self-closing element used as block element', () => {
-  return t(
-    null,
-    '<path><animateTransform /></path>',
-    '<path><animateTransform /></path>'
-  );
-});
+it('should detect if self-closing element used as block element', () => t(
+  null,
+  '<path><animateTransform /></path>',
+  '<path><animateTransform /></path>'
+));
 
