55d47d107ac09b4b0de66e74b4bf0e01877cf5fe
kisenka <qtuzov@gmail.com>
2017-03-24 22:16:28 +0300

    Add test utils

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+const processor = require('../src/posthtml-svg-mode');
+
+exports.setupPluginTest = (plugin = null) => {
+  return (options, input, expected) => {
+    const args = plugin ? [plugin(options !== null ? options : undefined)] : tree => tree;
+
+    return processor(args)
+      .process(input)
+      .then(result => expect(result.toString()).toEqual(expected));
+  };
+};
