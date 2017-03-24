e2c0f3fb392c58271bed537b585aa3adcc8c1108
kisenka <qtuzov@gmail.com>
2017-03-24 22:47:40 +0300

    svg-baker: add xlink ns to raster-to-svg transformer

--- src/svg-baker/lib/transformations/raster-to-svg.js
+++ src/svg-baker/lib/transformations/raster-to-svg.js
@@ -1,5 +1,5 @@
 const getImageSize = require('image-size');
-const { svg } = require('../namespaces');
+const { svg, xlink } = require('../namespaces');
 
 /**
  * @param {Buffer} buffer
@@ -9,10 +9,11 @@ function rasterToSVG(buffer) {
   const info = getImageSize(buffer);
   const { width, height, type } = info;
   const defaultNS = `${svg.name}="${svg.value}"`;
+  const xlinkNS = `${xlink.name}="${xlink.value}"`;
   const data = `data:image/${type};base64,${buffer.toString('base64')}`;
 
   return [
-    `<svg ${defaultNS} width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">`,
+    `<svg ${defaultNS} ${xlinkNS} width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">`,
     `<image xlink:href="${data}" width="${width}" height="${height}" />`,
     '</svg>'
   ].join('');
