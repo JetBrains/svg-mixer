fb79de677451c720255c7767e1766c7c55d0db5e
kisenka <qtuzov@gmail.com>
2017-04-27 17:59:41 +0300

    Update sprite tests & test data

--- packages/svg-baker-runtime/test/browser-sprite.test.js
+++ packages/svg-baker-runtime/test/browser-sprite.test.js
@@ -1,14 +1,22 @@
 import Sprite from '../src/browser-sprite';
-import SpriteSymbol from '../src/symbol';
+import SpriteSymbol from '../src/browser-symbol';
 import {
   parse,
   wrapInSvgString,
+  arrayFrom,
   dispatchCustomEvent
 } from '../src/utils';
 
-function createBaseTag() {
+/**
+ * @type {Array}
+ */
+const symbolsFixtures = require('./fixtures/data.json');
+
+function createBaseTag(href) {
+  const baseTagHref = href || window.location.href.split('#')[0];
+
   const baseTag = document.createElement('base');
-  baseTag.setAttribute('href', window.location.href.split('#')[0]);
+  baseTag.setAttribute('href', baseTagHref);
   document.querySelector('head').appendChild(baseTag);
 }
 
@@ -27,13 +35,6 @@ function mockAngular() {
   };
 }
 
-const symbolData = {
-  id: 'twitter',
-  use: 'twitter-usage',
-  viewBox: '0 0 273.4 222.2',
-  content: '<symbol viewBox="0 0 273.4 222.2" id="twitter"><path d="M273.4 26.3c-10.1 4.5-20.9 7.5-32.2 8.8 11.6-6.9 20.5-17.9 24.7-31-10.9 6.4-22.9 11.1-35.7 13.6A55.919 55.919 0 0 0 189.3 0c-31 0-56.1 25.1-56.1 56.1 0 4.4.5 8.7 1.5 12.8C88 66.5 46.7 44.2 19 10.3c-4.8 8.3-7.6 17.9-7.6 28.2 0 19.5 9.9 36.6 25 46.7-9.2-.3-17.8-2.8-25.4-7v.7c0 27.2 19.3 49.8 45 55-4.7 1.3-9.7 2-14.8 2-3.6 0-7.1-.4-10.6-1 7.1 22.3 27.9 38.5 52.4 39-19.2 15-43.4 24-69.7 24-4.5 0-9-.3-13.4-.8 24.8 15.9 54.3 25.2 86 25.2 103.2 0 159.6-85.5 159.6-159.6 0-2.4-.1-4.9-.2-7.3 11.1-8 20.6-17.9 28.1-29.1z" /></symbol>'
-};
-
 describe('svg-baker-runtime/browser-sprite', () => {
   describe('constructor()', () => {
     it('should assign default config properly', () => {
@@ -45,41 +46,25 @@ describe('svg-baker-runtime/browser-sprite', () => {
       sprite.isMounted.should.be.false;
     });
 
-    it('should autodetect when base tag fix needed', () => {
-      let config;
+    it.only('should sync sprite urls with base tag url when sprite is mounted', () => {
+      const baseUrl = window.location.href;
+      createBaseTag(baseUrl);
 
-      config = new Sprite().config;
-      expect(config.baseFix).to.be.false;
+      const sprite = new Sprite();
+      sprite.add(new SpriteSymbol(symbolsFixtures[0]));
 
-      createBaseTag();
-      config = new Sprite().config;
-      expect(config.baseFix).to.be.true;
-      removeBaseTag();
+      const node = sprite.mount();
+      const elemsWithUrl = arrayFrom(node.querySelectorAll('[fill^="url("]'));
 
-      createBaseTag();
-      config = new Sprite({ baseFix: false }).config;
-      expect(config.baseFix).to.be.false;
-      removeBaseTag();
+      elemsWithUrl.forEach((elem) => {
+        const fill = elem.getAttribute('fill');
+        const expectedIndex = 0;
+        const actualIndex = fill.indexOf(`url(${baseUrl}`);
+        actualIndex.should.be.equal(expectedIndex);
+      });
     });
 
-    it('should autodetect when Angular base tag fix needed', () => {
-      let config;
-
-      config = new Sprite().config;
-      expect(config.angularBaseFix).to.be.false;
-
-      window.angular = mockAngular();
-      config = new Sprite().config;
-      expect(config.angularBaseFix).to.be.true;
-      delete window.angular;
-
-      window.angular = mockAngular();
-      config = new Sprite({ angularBaseFix: false }).config;
-      expect(config.angularBaseFix).to.be.false;
-      delete window.angular;
-    });
-
-    it('should subscribe to location updated event and call `updateUrls` when it occurs', () => {
+    it('should subscribe to locationChange event and call updateUrls when it occurs', () => {
       const sprite = new Sprite();
       const updateUrls = sinon.spy(sprite, 'updateUrls');
 
@@ -88,6 +73,43 @@ describe('svg-baker-runtime/browser-sprite', () => {
       updateUrls.should.have.been.calledOnce;
 
       updateUrls.restore();
+    });
+  });
+
+  describe('autoConfigure()', () => {
+    it('should detect when base tag fix needed', () => {
+      const prop = 'syncUrlsWithBaseTag';
+      let config;
+
+      config = new Sprite().config;
+      expect(config[prop]).to.be.false;
+
+      createBaseTag();
+      config = new Sprite().config;
+      expect(config[prop]).to.be.true;
+      removeBaseTag();
+
+      createBaseTag();
+      config = new Sprite({ [prop]: false }).config;
+      expect(config[prop]).to.be.false;
+      removeBaseTag();
+    });
+
+    it('should detect when Angular base tag fix needed', () => {
+      const prop = 'locationChangeAngularEmitter';
+      let config;
+
+      config = new Sprite().config;
+      expect(config[prop]).to.be.false;
+
+      window.angular = mockAngular();
+      config = new Sprite().config;
+      expect(config[prop]).to.be.true;
+      delete window.angular;
+    });
+
+    it('should detect when Firefox gradients fix needed', () => {
+      // TODO
     });
   });
 
@@ -100,7 +122,7 @@ describe('svg-baker-runtime/browser-sprite', () => {
 
     it('should update urls in sprite symbols and in references', () => {
       const sprite = new Sprite();
-      sprite.add(new SpriteSymbol(symbolData));
+      sprite.add(new SpriteSymbol(symbolsFixtures[0]));
       sprite.mount();
 
       const ref = parse(wrapInSvgString('<use xlink:href="#twitter"></use>'));
