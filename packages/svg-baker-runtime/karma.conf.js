50e5f4a51985f7daffde94d96dc850a6aeb2484f
kisenka <qtuzov@gmail.com>
2017-04-27 13:29:22 +0300

    Setup mocha reporter in karma

--- packages/svg-baker-runtime/karma.conf.js
+++ packages/svg-baker-runtime/karma.conf.js
@@ -23,7 +23,11 @@ module.exports = (config) => {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
+
+    mochaReporter: {
+      showDiff: true
+    },
 
     // web server port
     port: 9876,
