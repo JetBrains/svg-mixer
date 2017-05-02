ffa0ccc9dfc36499921c9d1f7c4ec74cd29ee576
kisenka <qtuzov@gmail.com>
2017-05-02 18:53:33 +0300

    Drop .only test modifier in prefix-style-selectors

--- packages/svg-baker/test/transformations/prefix-style-selectors.test.js
+++ packages/svg-baker/test/transformations/prefix-style-selectors.test.js
@@ -1,10 +1,9 @@
-/* global utils */
 const plugin = require('../../lib/transformations/prefix-style-selectors');
 
 const t = utils.setupPluginTest(plugin);
 
 describe('svg-baker/transformations/prefix-style-selectors', () => {
-  it.only('should work', () => t(
+  it('should work', () => t(
     '.qwe',
     '<svg><defs><style>.a {} .b .c {}</style></defs></svg>',
     '<svg><defs><style>.qwe .a {} .qwe .b .c {}</style></defs></svg>'
