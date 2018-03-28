987bda630b13e1162541dc927bd4fcd433a2d6cd
kisenka <qtuzov@gmail.com>
2018-03-28 13:41:20 +0700

    feat: add more handy package entrypoint

---
+++ packages/postsvg/index.js
@@ -0,0 +1,13 @@
+const parse = require('./lib/parser');
+const Processor = require('./lib/processor');
+const render = require('./lib/renderer');
+const Tree = require('./lib/tree');
+
+module.exports = function postsvg(plugins) {
+  return new Processor(plugins);
+};
+
+module.exports.parse = parse;
+module.exports.Processor = Processor;
+module.exports.render = render;
+module.exports.Tree = Tree;
