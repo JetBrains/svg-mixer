ada3fb88b9a0b96a25805715156cc0cff64bf382
kisenka <qtuzov@gmail.com>
2018-04-02 14:39:21 +0300

    WIP: refactor

--- packages/svg-baker/lib/utils/format-number.js
+++
@@ -1,4 +0,0 @@
-module.exports = function formatNumber(num, decimalPlaces = 2) {
-  const hasDecimals = num % 1 !== 0;
-  return hasDecimals ? parseFloat(num).toFixed(decimalPlaces) : Math.round(num);
-};
--- packages/svg-baker/lib/utils/index.js
+++ packages/svg-baker/lib/utils/index.js
@@ -1,7 +1,6 @@
 module.exports.calculateSymbolPosition = require('./calculate-symbol-position');
 module.exports.createSprite = require('./create-sprite');
 module.exports.createImageFromFile = require('./create-image-from-file');
-module.exports.formatNumber = require('./format-number');
 module.exports.getBasename = require('./get-basename');
 module.exports.glob = require('./glob');
 module.exports.SpriteValue = require('./sprite-value');
