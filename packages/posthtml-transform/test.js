c70ad480049884f7690255b145a2b56b4d54b11d
kisenka <qtuzov@gmail.com>
2018-04-17 11:41:02 +0300

    refactor: tests

--- packages/posthtml-transform/test.js
+++ packages/posthtml-transform/test.js
@@ -4,54 +4,48 @@ const plugin = require('.');
 const t = utils.testPlugin(plugin);
 const image = utils.getFixture('twitter.svg');
 
-const cases = [
-  {
-    name: 'config: Object',
-    params: { attr: 'fill', value: 'red' }
-  },
-  {
-    name: 'config: Array<Object|Function>',
-    params: [{ attr: 'fill', value: 'red' }]
-  },
-  {
-    name: 'config: Function',
-    testFunc: async () => {
-      const tree = utils.parse(image);
-      const transformer = jest.fn(node => node.attrs.fill = 'red');
-      const res = await t(transformer, tree, { skipParse: true });
-
-      expect(transformer).toHaveBeenCalledTimes(2);
-      expect(res).toMatchSnapshot();
-    }
-  },
-  {
-    name: 'config: Array<Function>',
-    params: node => node.tag === 'svg' && (node.attrs.foo = 'bar')
-  },
-  {
-    name: 'config: throws if not Object|Array<Object|Function>|function',
-    params: { attr: 'fill', value: 'red' },
-    testFunc: async () => await expect(t('invalid config', image)).rejects.toThrowError()
-  },
-  {
-    name: 'should process only selector nodes',
-    params: { attr: 'fill', value: 'red', selector: 'path' }
-  },
-  {
-    name: 'should rename tag',
-    params: { selector: 'svg', tag: 'symbol' }
-  }
-];
-
-for (const item of cases) {
-  const testFunc = item.testFunc || (async () => {
-    expect(await t(item.params, item.image || image))
-      .toMatchSnapshot(item.snapshotName);
+describe('plugin', () => {
+  it('config: Object', async () => {
+    const params = { attr: 'fill', value: 'red' };
+    expect(await t(params, image)).toMatchSnapshot();
   });
 
-  if (item.only) {
-    it.only(item.name, testFunc);
-  } else {
-    it(item.name, testFunc);
-  }
-}
+  it('config: Array<Object|Function>', async () => {
+    const params = [{ attr: 'fill', value: 'red' }];
+    expect(await t(params, image)).toMatchSnapshot();
+  });
+
+  it('config: Function', async () => {
+    const tree = utils.parse(image);
+    const transformer = jest.fn(node => node.attrs.fill = 'red');
+    const res = await t(transformer, tree, { skipParse: true });
+
+    expect(transformer).toHaveBeenCalledTimes(2);
+    expect(res).toMatchSnapshot();
+  });
+
+  it('config: Array<Function>', async () => {
+    const params = node => node.tag === 'svg' && (node.attrs.foo = 'bar');
+    expect(await t(params(), image)).toMatchSnapshot();
+  });
+
+  it('config: throws if not Object|Array<Object|Function>|function', async () => {
+    await expect(t('invalid config', image)).rejects.toThrowError();
+  });
+
+  it('should process only selector nodes', async () => {
+    const params = { attr: 'fill', value: 'red', selector: 'path' };
+    expect(await t(params, image)).toMatchSnapshot();
+  });
+
+  it('should rename tag', async () => {
+    const params = { selector: 'svg', tag: 'symbol' };
+    expect(await t(params, image)).toMatchSnapshot();
+  });
+});
+
+describe('getTransformerParams', () => {
+  it.only('should work!', () => {
+    getTransformerParams(`qwe=${encodeURIComponent('#fff path')}`);
+  });
+});
