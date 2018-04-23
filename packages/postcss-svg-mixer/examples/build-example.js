1760e81f8e7b503f16f5aad408773674f635b14a
kisenka <qtuzov@gmail.com>
2018-04-23 12:38:39 +0300

    docs: add autogenerated readme in each example

--- packages/postcss-svg-mixer/examples/build-example.js
+++ packages/postcss-svg-mixer/examples/build-example.js
@@ -3,6 +3,25 @@ const { resolve } = require('path');
 const { readFile, outputFile } = require('fs-extra');
 const postcss = require('postcss');
 
+function generateReadme(input, output, sprite) {
+  return `
+**Input**
+\`\`\`css
+${input}
+\`\`\`
+
+**Output**
+\`\`\`css
+${output}
+\`\`\`
+
+**Sprite**
+\`\`\`svg
+${sprite}
+\`\`\`
+`;
+}
+
 module.exports = async (name, plugins = []) => {
   const exampleDir = resolve(__dirname, name);
   const inputPath = resolve(exampleDir, 'input.css');
@@ -13,7 +32,9 @@ module.exports = async (name, plugins = []) => {
     .process(inputContent, { from: inputPath });
 
   const sprite = result.messages.find(m => m.kind === 'sprite');
+  const readme = generateReadme(inputContent, result.css, sprite.content);
 
   await outputFile(resolve(outputDir, 'output.css'), result.css);
   await outputFile(resolve(outputDir, sprite.filename), sprite.content);
+  await outputFile(resolve(exampleDir, 'README.md'), readme);
 };
