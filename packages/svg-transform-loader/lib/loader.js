d9608a75f056bcf0941d8dab9b11ae1653f32c81
kisenka <qtuzov@gmail.com>
2018-07-20 15:10:21 +0300

    WIP: convert alpha colors

--- packages/svg-transform-loader/lib/loader.js
+++ packages/svg-transform-loader/lib/loader.js
@@ -7,8 +7,33 @@ const merge = require('merge-options');
 
 const defaultConfig = {
   raw: true,
-  transformQuery: null
+  transformQuery: null,
+  convertAlphaColors: false
 };
+
+/**
+ * @param {string} value
+ * @return {{space: string, alpha: number, values: number[]}|null}
+ */
+function parseColor(value) {
+  const parsed = require('color-parse')(value);
+  return parsed.space && !isEmpty(parsed.values) ? parsed : null;
+}
+
+function convertAlphaColors(query) {
+  const fillParsed = query.fill ? parseColor(query.fill) : null;
+  const strokeParsed = query.stroke ? parseColor(query.stroke) : null;
+
+  if (fillParsed && fillParsed.alpha < 1) {
+    query.fill = `${fillParsed.space}(${fillParsed.values.join(', ')})`;
+    query['fill-opacity'] = fillParsed.alpha.toString();
+  }
+
+  if (strokeParsed && strokeParsed.alpha < 1) {
+    query.stroke = `${strokeParsed.space}(${strokeParsed.values.join(', ')})`;
+    query['stroke-opacity'] = strokeParsed.alpha.toString();
+  }
+}
 
 function generateResult(content, raw = defaultConfig.raw) {
   return raw ? content : `module.exports = ${JSON.stringify(content)}`;
@@ -21,22 +46,26 @@ module.exports = function (content, map) {
   }
 
   const callback = this.async();
-  const loaderOpts = merge(defaultConfig, getOptions(this) || {});
+  const opts = merge(defaultConfig, getOptions(this) || {});
   const query = this.resourceQuery ? parseQuery(this.resourceQuery) : null;
 
   if (!query || isEmpty(query)) {
-    return callback(null, generateResult(content, loaderOpts.raw), map);
+    return callback(null, generateResult(content, opts.raw), map);
   }
 
-  if (typeof loaderOpts.transformQuery === 'function') {
-    loaderOpts.transformQuery(query);
+  if (typeof opts.transformQuery === 'function') {
+    opts.transformQuery(query);
+  }
+
+  if (opts.convertAlphaColors && (query.fill || query.stroke)) {
+    const color = parseColor();
   }
 
   postsvg()
     .use(transformPlugin(stringifyQuery(query)))
     .process(content)
     .then(res => {
-      callback(null, generateResult(res.svg, loaderOpts.raw), map, {
+      callback(null, generateResult(res.svg, opts.raw), map, {
         ast: res.tree
       });
     })
