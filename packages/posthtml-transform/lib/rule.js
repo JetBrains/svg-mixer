c80eb05288ef27ec4d0f8bfa2de33589d00cc4b1
kisenka <qtuzov@gmail.com>
2019-02-12 20:49:30 +0300

    fix: select node value properly

--- packages/posthtml-transform/lib/rule.js
+++ packages/posthtml-transform/lib/rule.js
@@ -3,23 +3,39 @@ const { strip: stripSlashes } = require('slashes');
 const unquote = require('unquote');
 
 const CSS_VALUE_TYPE_REGEXP = /^(word|func|number|string)$/;
+const NODE_TYPES = {
+  WORD: 'word',
+  FUNCTION: 'func',
+  NUMBER: 'number',
+  STRING: 'string',
+  COMMA: 'comma'
+};
 
 module.exports = class Rule {
   static parseValue(val) {
-    const value = unquote(stripSlashes(val));
+    const value = stripSlashes(val);
     const root = valuesParser(value).parse();
     const values = [];
     let counter = 0;
 
     root.first.nodes.forEach(node => {
-      if (CSS_VALUE_TYPE_REGEXP.test(node.type)) {
-        if (!values[counter]) {
-          values[counter] = [];
-        }
-        const value = unquote(node.toString()).trim();
-        values[counter].push(value);
-      } else if (node.type === 'comma') {
-        counter++;
+      switch (node.type) {
+        case NODE_TYPES.WORD:
+        case NODE_TYPES.FUNCTION:
+        case NODE_TYPES.NUMBER:
+        case NODE_TYPES.STRING:
+          if (!values[counter]) {
+            values[counter] = [];
+          }
+          const nodeValue = node.type === NODE_TYPES.FUNCTION
+            ? node.toString().trim()
+            : unquote(node.value).trim();
+          values[counter].push(nodeValue);
+          break;
+
+        case NODE_TYPES.COMMA:
+          counter++;
+          break;
       }
     });
 
