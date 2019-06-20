bc9bbd251d7cf801f698bbbbc8e4dc9e891f1df2
Egor Blinov <eho31@yandex.ru>
2019-06-20 18:08:55 +0300

    feat: add clipPath & mask to "move-from-symbol-to-root" transform defaults

--- packages/svg-baker/lib/transformations/move-from-symbol-to-root.js
+++ packages/svg-baker/lib/transformations/move-from-symbol-to-root.js
@@ -3,7 +3,7 @@ const clone = require('clone');
 
 // Fixes Firefox bug https://bugzilla.mozilla.org/show_bug.cgi?id=353575
 const defaultConfig = {
-  tags: ['linearGradient', 'radialGradient', 'pattern']
+  tags: ['linearGradient', 'radialGradient', 'pattern', 'clipPath', 'mask']
 };
 
 function moveFromSymbolToRoot(config = null) {
