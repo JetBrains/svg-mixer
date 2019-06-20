da75a5577bde27b2765e5adbdaf35feb00ca4ba4
Egor Blinov <eho31@yandex.ru>
2019-06-20 18:12:16 +0300

    test: update svg-baker transformations tests

--- packages/svg-baker/test/transformations/move-from-symbol-to-root.test.js
+++ packages/svg-baker/test/transformations/move-from-symbol-to-root.test.js
@@ -5,7 +5,7 @@ const t = utils.setupPluginTest(plugin);
 describe('svg-baker/transformations/move-from-symbol-to-root', () => {
   it('should work', () => t(
     undefined,
-    '<svg><symbol><defs><linearGradient></linearGradient></defs></symbol></svg>',
-    '<svg><symbol><defs></defs></symbol><linearGradient></linearGradient></svg>'
+    '<svg><symbol><defs><linearGradient></linearGradient><mask></mask><clipPath></clipPath></defs></symbol></svg>',
+    '<svg><symbol><defs></defs></symbol><linearGradient></linearGradient><mask></mask><clipPath></clipPath></svg>'
   ));
 });
