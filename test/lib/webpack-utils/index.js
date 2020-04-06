0f944e696bff7647d32b2ecd63ac742137d0e052
kisenka <qtuzov@gmail.com>
2020-04-06 19:37:22 +0300

    test: getAssets util now returns an object

--- test/lib/webpack-utils/index.js
+++ test/lib/webpack-utils/index.js
@@ -3,10 +3,12 @@ module.exports.createCompiler = require('./create-compiler');
 module.exports.featureDetector = require('./feature-detector');
 module.exports.RemoveAssetsPlugin = require('./remove-assets-plugin');
 
-module.exports.getAssets = compilation =>
-  Object
-    .keys(compilation.assets)
-    .map(name => ({
-      name,
-      content: compilation.assets[name].source().toString().trim()
-    }));
+/**
+ * @param {webpack.Compilation} compilation
+ * @returns {Object<{name: string, content: string}>}
+ */
+module.exports.getAssets = (compilation) =>
+  Object.keys(compilation.assets).reduce((acc, name) => {
+    acc[name] = compilation.assets[name].source().toString().trim();
+    return acc;
+  }, {});
