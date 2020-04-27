48724b9a8b9bf487059e01199db38c59078dc312
kisenka <qtuzov@gmail.com>
2020-04-27 18:57:58 +0300

    refactor: drop redundant util

--- test/lib/webpack-utils/index.js
+++ test/lib/webpack-utils/index.js
@@ -2,13 +2,3 @@ module.exports.createBaseConfig = require('./create-base-config');
 module.exports.createCompiler = require('./create-compiler');
 module.exports.featureDetector = require('./feature-detector');
 module.exports.RemoveAssetsPlugin = require('./remove-assets-plugin');
-
-/**
- * @param {webpack.Compilation} compilation
- * @returns {Object<{name: string, content: string}>}
- */
-module.exports.getAssets = (compilation) =>
-  Object.keys(compilation.assets).reduce((acc, name) => {
-    acc[name] = compilation.assets[name].source().toString().trim();
-    return acc;
-  }, {});
